79133b3dbe7dc4953258246d8d91ef7244a25d3d
Isamar Bonillo Pérez <151036134+ibonper0708@users.noreply.github.com>
2024-02-16 11:56:42 +0100

    Add files via upload

---
+++ 10/js/script.js
@@ -0,0 +1,8 @@
+function Califica() {
+        let alumnos = ["Juan", "María", "Elisa", "Pepe"];
+        let notas  = [];
+            notas[0]=parseInt(prompt("¿Cuál es la nota de " + alumnos[0] + "?"));
+            notas[1]=parseInt(prompt("¿Cuál es la nota de " + alumnos[1] + "?"));
+            notas[2]=parseInt(prompt("¿Cuál es la nota de " + alumnos[2] + "?"));
+            notas[3]=parseInt(prompt("¿Cuál es la nota de " + alumnos[3] + "?"));
+    }
